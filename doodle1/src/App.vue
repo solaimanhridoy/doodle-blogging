0c6a4067289701187775ee408b0e4c122635e29b
solaiman-hridoy <libnum10@gmail.com>
2021-03-02 22:09:46 +0600

    Initial Files and ADD login, signup form

---
+++ doodle1/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <HelloWorld msg="Welcome to Doodle"/>
+    <SignUp/>
+  
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import SignUp from './components/SignUp.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    SignUp
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
